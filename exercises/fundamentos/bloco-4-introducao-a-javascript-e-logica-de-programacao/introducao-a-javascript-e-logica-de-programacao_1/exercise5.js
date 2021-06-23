d046e7c95b566aa0fb597c3625a64d44bdf04a44
Bruno Feu <brunofeu89@gmail.com>
2021-06-23 17:12:54 -0300

    adiciona exercicio5

---
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_1/exercise5.js
@@ -0,0 +1,13 @@
+let angleA = 1;
+let angleB = 89;
+let angleC = 90;
+
+if (angleA <= 0 ||  angleB <= 0 ||  angleC <= 0) {
+	console.log("Angulo não pode ser 0");
+} 
+	else if (angleA + angleB + angleC == 180){
+  	console.log("true");
+}
+ 	else {
+  	console.log("false");
+}
