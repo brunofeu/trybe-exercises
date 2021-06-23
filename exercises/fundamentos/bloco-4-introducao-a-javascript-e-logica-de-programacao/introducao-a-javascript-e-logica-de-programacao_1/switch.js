eb29961e3b54550a9dee07c2b49f3f236ec36b9a
Bruno Feu <brunofeu89@gmail.com>
2021-06-23 15:30:35 -0300

    adiciona exercicio switch

---
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_1/switch.js
@@ -0,0 +1,18 @@
+let notaPessoa = 'lista';
+
+switch (notaPessoa){
+    case "aprovado":
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+
+    case ("lista"):
+    console.log('Você está em nossa lista de espera');
+    break;
+
+    case ("reprovado"):
+    console.log('Você foi reprovada(o)');
+    break;
+
+    default: console.log('resultado inválido')
+}
+
