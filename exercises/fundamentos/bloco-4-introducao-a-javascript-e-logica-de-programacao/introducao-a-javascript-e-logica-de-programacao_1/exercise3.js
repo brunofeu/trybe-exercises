a5fbe4b0897e53ac120664279cb3e388aa9dbfd5
Bruno Feu <brunofeu89@gmail.com>
2021-06-23 15:54:08 -0300

    adiciona exercicio4

---
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_1/exercise3.js
@@ -0,0 +1,13 @@
+let a = 31;
+let b = 31;
+let c = 31;
+
+if (a >= b && a >= c) {
+    console.log('O número ' + a + ' é maior')
+}
+else if (b >= a && b >= c) {
+    console.log('O número ' + b + ' é maior')
+}
+else if (c >= a && c >= b) {
+    console.log('O número ' + c + ' é maior')
+}
