3f03e5fe4a770cbbac2de41ab9a0d9a8cac754ca
Bruno Feu <brunofeu89@gmail.com>
2021-06-24 16:03:23 -0300

    adiciona bonus2

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_2/exercises8.js
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_2/exercises8.js
@@ -1,5 +1,5 @@
-let array= [];
+let numbers= [];
 for (let index = 0; index < 25; index += 1){
-    array[index] = index +1;
+    numbers[index] = index +1;
 }
-console.log(array);
+console.log(numbers);
--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_2/exercises9.js
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_2/exercises9.js
@@ -1,8 +1,8 @@
 let array= [];
 for (let index = 0; index < 25; index += 1){
-    array[index] = index +1;
+    numbers[index] = index +1;
 }
 
- for (arr of array){
+ for (arr of numbers){
      console.log(arr/2)
  }
