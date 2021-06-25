f4dc42c5b6077823b37b6127d3d79cb8b1b36821
Bruno Feu <brunofeu89@gmail.com>
2021-06-25 14:58:16 -0300

    adiciona exercicio1

---
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_3/exercise1.js
@@ -0,0 +1,9 @@
+let n = 10;
+let resultado='';
+
+for (let indexColuna=0; indexColuna<n;indexColuna+=1){
+    resultado += '*';
+}
+for (indexLinha=0; indexLinha<n; indexLinha+=1){
+    console.log(resultado);
+}
