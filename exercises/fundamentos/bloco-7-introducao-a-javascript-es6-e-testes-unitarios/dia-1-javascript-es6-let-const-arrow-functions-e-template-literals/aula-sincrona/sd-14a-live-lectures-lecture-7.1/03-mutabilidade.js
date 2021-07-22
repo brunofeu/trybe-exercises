f97447a6a1f1a62a7812134f5d976f5986b91920
Bruno Feu <brunofeu89@gmail.com>
2021-07-22 16:30:31 -0300

    adiciona exercicios da aula sincrona

---
+++ exercises/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/aula-sincrona/sd-14a-live-lectures-lecture-7.1/03-mutabilidade.js
@@ -0,0 +1,17 @@
+var nome = 'Alexandre';
+console.log('Meu nome é:', nome);
+
+nome = 'Marcia';
+console.log('Meu nome é:', nome);
+
+// let nome = 'Alexandre';
+// console.log('Meu nome é:', nome);
+
+// nome = 'Marcia';
+// console.log('Meu nome é:', nome);
+
+// const nome = 'Alexandre';
+// console.log('Meu nome é:', nome);
+
+// nome = 'Marcia';
+// console.log('Meu nome é:', nome);
---
+++ exercises/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/aula-sincrona/sd-14a-live-lectures-lecture-7.1/06-operador-ternario.js
@@ -0,0 +1,9 @@
+let idade = 65;
+
+if(idade >= 18 ){
+    console.log('Maior  de idade!');
+}else{
+    console.log('Menor  de idade!');
+}
+
+// idade >= 18 ? console.log('Maior  de idade!') : console.log('Menor  de idade!');
