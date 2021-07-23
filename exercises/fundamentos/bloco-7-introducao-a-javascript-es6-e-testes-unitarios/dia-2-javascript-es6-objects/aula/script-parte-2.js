5fb1b381ee82df4525ab0c7ffce36191a6375a4a
Bruno Feu <brunofeu89@gmail.com>
2021-07-23 16:26:49 -0300

    adiciona os exercicios da aula 7.2

---
+++ exercises/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/aula/script-parte-2.js
@@ -0,0 +1,38 @@
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo'
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const show = (obj) => {
+// Object.keys(obj);
+const value = Object.keys(obj);
+for (index in value){
+  console.log(`${value[index]}, Nível: ${obj[value[index]]}`);
+} 
+}
+show(student1);
+console.log(' ')
+show(student2);
+
+// const listSkills = (student) => {
+//   const arrayOfSkills = Object.keys(student);
+//   for(index in arrayOfSkills){
+//     console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+//   }
+// };
+
+// console.log('Estudante 1');
+// listSkills(student1);
+
+// console.log('Estudante 2');
+// listSkills(student2);
