fbb5e05aba558135255f97b2981a53b6cf6f6c50
Bruno Feu <brunofeu89@gmail.com>
2021-08-03 16:19:06 -0300

    add exercise "default"

---
+++ exercises/fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/aula/exercise-default-destructuring.js
@@ -0,0 +1,19 @@
+const getNationality = ({ firstName, nationality = 'Brazilian'}) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+// const {nationality = 'Brazilian'} = person;
+// const { nationality = "Brazilian" } = otherPerson;
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+
