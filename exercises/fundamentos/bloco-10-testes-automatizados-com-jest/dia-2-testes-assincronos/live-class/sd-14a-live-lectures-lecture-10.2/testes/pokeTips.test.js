7477a3a5c1acb89ded0fea2e52a0663307b6de02
brunofeu <brunofeu89@gmail.com>
2021-08-18 18:13:38 -0300

    add live-class exercises

--- exercises/fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-testes-assincronos/live-class/sd-14a-live-lectures-lecture-10.2/testes/pokeTips.test.js
+++ exercises/fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-testes-assincronos/live-class/sd-14a-live-lectures-lecture-10.2/testes/pokeTips.test.js
@@ -2,11 +2,11 @@ const pokeTips = require("../js/pokeTips");
 
 // Validar as respostas da função pokeTips usando Jest
 
-test('Retornar o melhor pokemon para começar o jogo', () =>{
+test('Retornar o melhor pokemon para começar o jogo', (done) => {
   const callback = (dado) => {
     expect(dado).toBe('Charmander é o melhor pokémon para começar.')
-    // done();
+    done();
   }
-  pokeTips(callback)
+  return pokeTips(callback)
 })
 
