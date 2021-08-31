d15ccffa740fc5eb03bf863778412fe1205d3dea
brunofeu <brunofeu89@gmail.com>
2021-08-31 14:49:18 -0300

    add exercises class Task list

--- exercises/frond-end/bloco-11-introducao-react/exercise-app/src/Task.js
+++ exercises/frond-end/bloco-11-introducao-react/exercise-app/src/Task.js
@@ -3,12 +3,15 @@ import React from "react";
 class Task extends React.Component {
   render() {
     const Task = (value) => <li>{value}</li>;
+    const compromisses = ["Aprender React", 'Treinar React', 'Estudar React', 'React é Maravilhoso!']
     return (
       <>
       {Task("Aprender React")}
       {Task ('Treinar React')}
       {Task ('Estudar React')}
       {Task ('React é Maravilhoso!')}
+      {<br></br>}
+      {compromisses.map((task) => Task(task))}
       </>
     )
   }
