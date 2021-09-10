a4914875997583b90e58c3809289e90969b9a3ac
brunofeu <brunofeu89@gmail.com>
2021-09-10 17:18:14 -0300

    add live-class exercises (cronometer)

---
+++ exercises/frond-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/live-class/cronometer/src/components/Cronometer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+
+class Cronometer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  componentDidMount() {
+    const ONE_SECOND = 1000;
+    setInterval(() => {
+      this.setState((prevState) => ({ seconds: prevState.seconds + 1}))
+    }, ONE_SECOND);
+  }
+
+  render() {
+    const { seconds } = this.state;
+
+    
+
+    return (
+      <h2>
+        {seconds}
+      </h2>
+    );
+  }
+}
+
+export default Cronometer;
