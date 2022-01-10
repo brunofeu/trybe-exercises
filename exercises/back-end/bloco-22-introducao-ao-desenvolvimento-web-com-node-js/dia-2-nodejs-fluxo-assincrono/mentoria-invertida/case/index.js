33c74655597c476e428b54272aa78cda5abe802e
brunofeu <brunofeu89@gmail.com>
2022-01-10 20:47:13 -0300

    adiciona mentoria invertida com promises

--- exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-2-nodejs-fluxo-assincrono/mentoria-invertida/case/index.js
+++ exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-2-nodejs-fluxo-assincrono/mentoria-invertida/case/index.js
@@ -29,12 +29,27 @@ const leitorCallback = () => {
             fs.writeFile('./arquivo3.json', JSON.stringify(obj), (err) => {
               if (err) return console.log(err.message);
               console.log('todos os arquivos foram salvos corretamente')
-            })
+            });
           });
-        })
+        });
       });
-    })
+    });
   });
+};
+
+// leitorCallback();
+
+
+const leitorPromises = (arquivo, nomeJson) => {
+  fs.promises.readFile(arquivo, 'utf-8')
+    .then((response) => {
+      const obj = Object.fromEntries(response.split('\n').map((linha) => linha.split('-')))
+      fs.promises.writeFile(`./${nomeJson}.json`, JSON.stringify(obj))
+    })
+    .catch((err) => console.log(err))
 }
 
-leitorCallback();
+leitorPromises('./arquivo1.txt', 'arquivo1-2');
+leitorPromises('./arquivo2.txt', 'arquivo2-2');
+leitorPromises('./arquivo3.txt', 'arquivo3-2');
+//AJUSTAR UM DEPENDENDO DO OUTRO
