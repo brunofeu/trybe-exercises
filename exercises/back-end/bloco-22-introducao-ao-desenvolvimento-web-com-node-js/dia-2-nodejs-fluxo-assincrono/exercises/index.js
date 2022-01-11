6c441480abcfd093fc041323ab9006fe42b1a7be
brunofeu <brunofeu89@gmail.com>
2022-01-11 16:43:15 -0300

    adiciona exercicios do course até o 4.3

---
+++ exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-2-nodejs-fluxo-assincrono/exercises/index.js
@@ -0,0 +1,61 @@
+// 1. Crie uma função que recebe três parâmetros retorna uma Promise .
+// Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo "Informe apenas números" .
+// Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro ( (a + b) * c ).
+// Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
+// Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
+
+const exercicio1 = (a, b, c) => {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+      return reject("Informe apenas números");
+    }
+
+    const math = ((a + b) * c);
+
+    if (math < 50 ) return reject('valor muito baixo');
+    
+    resolve(math)
+  })
+}
+
+// exercicio1(10, 45, 3)
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error));
+
+// exercicio1(10, 45, '3')
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error));
+
+// exercicio1(10, 2, 3)
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error));
+
+
+
+// 2. Escreva um código para consumir a função construída no exercício anterior.
+// Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe. Para gerar um número aleatório, utilize o seguinte trecho de código: Math.floor(Math.random() * 100 + 1) .
+// Chame a função do exercício anterior, passando os três números aleatórios como parâmetros.
+// Utilize then e catch para manipular a Promise retornada pela função:
+// Caso a Promise seja rejeitada, escreva na tela o motivo da rejeição.
+// Caso a Promise seja resolvida, escreva na tela o resultado do cálculo.
+
+const randomNumber= () => Math.floor(Math.random() * 100 + 1)
+
+// exercicio1(randomNumber(), randomNumber() , randomNumber())
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error));
+
+// 3. Reescreva o código do exercício anterior para que utilize async/await .
+// Lembre-se: a palavra chave await só pode ser utilizada dentro de funções async .
+
+const main = async () => {
+  try {
+    const result = await exercicio1(randomNumber(), randomNumber() , randomNumber())
+    console.log(result)
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// main();
+
