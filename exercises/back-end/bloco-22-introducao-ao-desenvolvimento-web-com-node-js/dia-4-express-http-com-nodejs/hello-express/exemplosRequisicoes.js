46b7c1645ef91a6e81c132b476bcc725cda1066d
brunofeu <brunofeu89@gmail.com>
2022-01-12 16:44:31 -0300

    adiciona exemplos de requisicoes pelo front-end

---
+++ exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-nodejs/hello-express/exemplosRequisicoes.js
@@ -0,0 +1,23 @@
+//No FrontEnd as requsições podem ser feitas como:
+
+// Requisição do tipo PUT
+fetch(`http://localhost:3001/recipes/2`, {
+  method: 'PUT',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    name: 'Macarrão ao alho e óleo',
+    price: 40
+  })
+});
+
+// Requisição do tipo DELETE
+fetch(`http://localhost:3001/recipes/4`, {
+  method: 'DELETE',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }
+});
