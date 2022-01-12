239787ded7edca80b212fbf14b12ce7c4f732f9e
brunofeu <brunofeu89@gmail.com>
2022-01-12 16:43:48 -0300

    adiciona exemplos de PUT e DELETE

--- exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-nodejs/hello-express/index2.js
+++ exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-nodejs/hello-express/index2.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
@@ -16,13 +19,23 @@ const drinks = [
   { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
-app.listen(3001, () => {
-  console.log('Aplicação ouvindo na porta 3001');
+
+app.get('/validateToken', function (req, res) {
+  const token = req.headers.authorization;
+  if (token.length !== 16) return res.status(401).json({message: 'Invalid Token!'});
+
+  res.status(200).json({message: 'Valid Token!'})
 });
 
 app.get('/recipes', (req, res) => {
   res.json(recipes);
 });
+
+app.post('/recipes', (req, res) => {
+  const { id, name, price, waitTime } = req.body;
+  recipes.push({ id, name, price, waitTime});
+  res.status(201).json({message: 'Recipe created successfully!'})
+})
 
 //---------Query String----------//
 
@@ -45,8 +58,16 @@ app.get('/recipes/:id', function (req, res) {
   res.status(200).json(recipe);
 });
 
+//------------DRINKS------------//
+
 app.get('/drinks', (req, res) => {
   res.send(drinks)
+})
+
+app.post('/drinks', (req, res) => {
+  const { id, name, price } = req.body;
+  drinks.push({ id, name, price });
+  res.status(201).json({message: 'Recipe created successfully!'});
 })
 
 //---------Query String----------//
@@ -67,4 +88,36 @@ app.get('/drinks/:id', (req, res) => {
 
   res.status(200).send(drink)
 })
+
+
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!'});
+
+  recipes.splice(recipeIndex, 1);
+  res.status(204).end();
+})
+
+
+app.all('*', (req, res) => {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
 
