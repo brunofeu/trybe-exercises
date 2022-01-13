b945cd854c3f100f21ccfe3ccaddf0fb99ddf6e4
brunofeu <brunofeu89@gmail.com>
2022-01-13 15:51:57 -0300

    adiciona aula do course

---
+++ exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/hello-express/errorHandlerExample.js
@@ -0,0 +1,43 @@
+//⚠️ Atenção ⚠️: Jamais devemos realizar a leitura de um arquivo do sistema 
+//de arquivos dessa forma. Concatenar parâmetros recebidos do usuário diretamente 
+//na chamada para qualquer método representa uma falha gigantesca de segurança. 
+//Vamos fazer isso aqui nesse momento para fins didáticos. 
+
+const app = require('express')();
+const rescue = require('express-rescue');
+const fs = require('fs/promises');
+
+// app.get('/:fileName', async (req, res, next) => {
+//   try {
+//       const file = await fs.readFile(`./${req.params.fileName}`);
+//       res.send(file.toString('utf-8'));
+//   } catch (e) {
+//       next(e);
+//   }
+// });
+
+app.get(
+  '/:fileName',[
+  rescue(async (req, res) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  }), 
+
+  (err, req, res, next) => {
+    if (err.code === 'ENOENT') {
+      const newError = new Error(err.message);
+      newError.code = 'file_not_found';
+      newError.status = 404;
+      return next(newError);
+    }
+
+    return next(err);
+  },
+
+  ]);
+
+app.use(function (err, req, res, next) {
+  res.status(500).json({ error: `Erro: ${err.message}` });
+});
+
+app.listen(3002);
