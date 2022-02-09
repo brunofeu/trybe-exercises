b71692e0d3974d61c9da4e443026c70399a5ff19
brunofeu <brunofeu89@gmail.com>
2022-02-09 20:36:33 -0300

    Remove arquivos de outros diretorios

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_2/bonus3.js
+++
@@ -1,7 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-numbers.push(2);
-let array =[];
-for (index = 0 ; index < numbers.length - 1; index += 1){
-    array[index] = numbers[index]*numbers[index + 1];  
-}
-console.log(array);
