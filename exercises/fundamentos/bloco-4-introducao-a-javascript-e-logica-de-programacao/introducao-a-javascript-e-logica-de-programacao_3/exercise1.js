f9c098eb2f78afe39866b3b9c554ca02de4c0ce0
brunofeu <brunofeu89@gmail.com>
2022-02-09 20:33:45 -0300

    Remove arquivos de outros diretorios

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_3/exercise1.js
+++
@@ -1,9 +0,0 @@
-let n = 10;
-let resultado='';
-
-for (let indexColuna=0; indexColuna<n;indexColuna+=1){
-    resultado += '*';
-}
-for (indexLinha=0; indexLinha<n; indexLinha+=1){
-    console.log(resultado);
-}
