8397851acef00a2bb1064286c0f20e759bbe90d8
brunofeu <brunofeu89@gmail.com>
2022-02-09 20:43:20 -0300

    Remove arquivos de outros diretorios

--- exercises/fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/aula/script.js
+++
@@ -1,7 +0,0 @@
-const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
-const prices = [2.99, 3.99, 1.5, 2];
-
-const listProducts = (product, price) => product.map((item, index) => ( { [item] : price[index]}) )
-
-
-console.log(listProducts(products, prices))
