e2cc22d6aa3fb7b84007be46e85c8e3ece6420e2
brunofeu <brunofeu89@gmail.com>
2022-02-09 20:34:29 -0300

    Remove arquivos de outros diretorios

--- exercises/fundamentos/bloco-5-introducao-a-javascript-projetos/dia-1-javascript-dom-e-seletores/aula/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByTagName('h1')[0].style.backgroundColor = 'green'
-document.getElementsByTagName('h1')[0].style.padding = '15px'
-document.getElementById('header-container').classList.= '0px'
-
-document.getElementsByTagName('h1')[0].style.backgroundColor = 'green'
