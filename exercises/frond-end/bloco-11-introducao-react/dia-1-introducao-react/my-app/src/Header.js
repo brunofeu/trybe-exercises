df72ca138ab4c1924b1e38cf8016620b778dddce
brunofeu <brunofeu89@gmail.com>
2022-02-10 14:18:24 -0300

    Remove diretorios de outras aulas

--- exercises/frond-end/bloco-11-introducao-react/dia-1-introducao-react/my-app/src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Header extends React.Component {
-  render() {
-    return <h1>Bloco 11.1 - Introdução ao React</h1>
-  }
-}
-
-export default Header
--- exercises/fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-testes-assincronos/live-class/sd-14a-live-lectures-lecture-10.2/js/pokeTips.js
+++
@@ -1,7 +0,0 @@
-const pokeTips = (callback) => {
-  setTimeout(() => {
-    callback('Charmander é o melhor pokémon para começar.');
-  }, 1000);
-};
-
-module.exports = pokeTips;
