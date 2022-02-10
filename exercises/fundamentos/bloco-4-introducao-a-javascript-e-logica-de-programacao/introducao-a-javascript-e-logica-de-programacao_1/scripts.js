4dc05d789fbd101d49a7ef502f75438c3f286384
brunofeu <brunofeu89@gmail.com>
2022-02-10 14:58:54 -0300

    Remove diretorios de outras aulas

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++
@@ -1,8 +0,0 @@
-const name = 'Bruno';
-const birthCity = 'Guarulhos';
-let birthYear = 1989;
-birthYear = 2030;
-birthCity = 'São Paulo';
-console.log(name);
-console.log(birthCity);
-console.log(birthYear);
--- exercises/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/aula-sincrona/sd-14a-live-lectures-lecture-7.1/06-operador-ternario.js
+++
@@ -1,9 +0,0 @@
-let idade = 65;
-
-if(idade >= 18 ){
-    console.log('Maior  de idade!');
-}else{
-    console.log('Menor  de idade!');
-}
-
-// idade >= 18 ? console.log('Maior  de idade!') : console.log('Menor  de idade!');
