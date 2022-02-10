4cac7eaa69021df76b085814b962d3653c8f70c1
brunofeu <brunofeu89@gmail.com>
2022-02-10 14:40:36 -0300

    Remove diretorios de outras aulas

--- exercises/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scriptforindex.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementById('botao');
-const cont = document.getElementById('cont');
-let clickCount = 0;
-botao.addEventListener('click', (event) => {
-  clickCount +=1;
-  cont.innerHTML = clickCount;
-})
-
