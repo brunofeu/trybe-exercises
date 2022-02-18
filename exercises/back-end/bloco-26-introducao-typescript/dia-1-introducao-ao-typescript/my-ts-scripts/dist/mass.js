3a0d0a921ae8307104ba396d912875c39dbbe914
brunofeu <brunofeu89@gmail.com>
2022-02-18 16:35:44 -0300

    add gitignore

--- exercises/back-end/bloco-26-introducao-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/mass.js
+++ exercises/back-end/bloco-26-introducao-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/mass.js
@@ -1,11 +1,10 @@
-"use strict";
-const units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
-const convert = (valor, base, conversao) => {
-    const forIndex = units.indexOf(base);
-    const toIndex = units.indexOf(conversao);
-    const expoent = (toIndex - forIndex);
-    const result = valor * Math.pow(10, expoent);
-    console.log(result, `${conversao}`);
+var units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
+var convert = function (valor, base, conversao) {
+    var forIndex = units.indexOf(base);
+    var toIndex = units.indexOf(conversao);
+    var expoent = (toIndex - forIndex);
+    var result = valor * Math.pow(10, expoent);
+    console.log(result, "".concat(conversao));
     return result;
 };
 convert(10, 'kg', 'mg');
